f4b5a4cbf5ea5466f99a1b1a5e151f4419c01af0
Edward Hale <47165540+edhale3@users.noreply.github.com>
2020-04-24 14:12:01 -0700

    Add comments
    Added comments

--- adobeIcon_script.ps1
+++ adobeIcon_script.ps1
@@ -1,10 +1,22 @@
+#To be run as admin in Powershell
+#Get computer name
 $computer = $env:computername
 
+#Condition on if this .lnk file already exists. If it exists, remove it to prepare for new file.
 if("\\$computer\C$\ProgramData\Microsoft\Windows\Start Menu\Programs\Acrobat Reader DC.lnk"){
 Remove-Item “\\$computer\C$\ProgramData\Microsoft\Windows\Start Menu\Programs\Acrobat Reader DC.lnk”}
 
+#New Object
 $WshShell = New-Object -ComObject WScript.Shell
+
+#Create the method to create shortcut
 $Shortcut = $WshShell.CreateShortcut("\\$computer\C$\ProgramData\Microsoft\Windows\Start Menu\Programs\Acrobat Reader DC.lnk")
+
+#Choose target path (this is where the program usually is installed)
 $Shortcut.TargetPath = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
+
+#Get the icon location. For Adobe Reader the shortcut is included in the executable. Simply added the .exe as the icon location will work.
 $Shortcut.IconLocation = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe,0"
+
+#Save changes
 $Shortcut.Save()
